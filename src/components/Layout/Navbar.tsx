686172951404b3694027d0dfaac49b3f44b4d40d
johnvesslyalti <altijohnvessly@gmail.com>
2025-09-24 18:25:19 +0530

    fix: redirected to the dashboard if user exists

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -27,7 +27,7 @@ export function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
-          <Link href="/" className="flex items-center space-x-2">
+          <Link href={status == 'authenticated' ? '/dashboard' : '/'} className="flex items-center space-x-2">
             <div className="w-10 h-10 bg-gradient-to-r from-purple-500 to-blue-500 rounded-xl flex items-center justify-center shadow-lg">
               <span className="text-white font-bold text-lg">TP</span>
             </div>
